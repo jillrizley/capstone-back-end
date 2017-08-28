ddab756d9147f3e7a586e286f90daa97dd13f360
jillrizley <jillian.rizley@gmail.com>
2017-08-28 15:56:50 -0400

    added photo controller and model

---
+++ app/models/photo.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const photoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  comments: {
+    type: String,
+    require: true
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+  }
+})
+
+const Photo = mongoose.model('Photo', photoSchema)
+
+module.exports = Photo
