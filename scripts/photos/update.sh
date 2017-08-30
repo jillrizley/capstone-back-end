1ac6d464cb61e31e96875d100729e3e73581cca8
jillrizley <jillian.rizley@gmail.com>
2017-08-30 15:31:16 -0400

    update to curl requests for photos to update and destroy

--- scripts/photos/update.sh
+++ scripts/photos/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API="http://localhost:4741"
-URL_PATH="/albums"
+URL_PATH="/photos"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
@@ -9,7 +9,7 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
-    "album": {
+    "photo": {
       "title": "'"${TITLE}"'"
       "comment": "'"${COMMENT}"'"
     }
